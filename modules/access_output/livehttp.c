636c052936523341a4822d6d49892720ae4577a8
Alaric Senat <dev.asenat@posteo.net>
2021-06-08 11:21:36 +0200

    cleanup httpd callbacks

--- modules/access_output/livehttp.c
+++ modules/access_output/livehttp.c
@@ -271,127 +271,15 @@ typedef struct
     httpd_host_t *http_host;
 } sout_access_out_sys_t;
 
+
 static int url_playlist_cb( httpd_callback_sys_t *data,
                             httpd_client_t *cl,
                             httpd_message_t *answer,
-                            const httpd_message_t *query )
-{
-    if ( !answer || !query || !cl )
-        return VLC_SUCCESS;
-
-    sout_access_out_sys_t *p_sys = (sout_access_out_sys_t *)data;
-
-    const hls_io *io = p_sys->p_playlist;
-
-    vlc_mutex_lock( &p_sys->playlist_lock );
-
-    if ( io == NULL )
-        goto err;
-
-    void *reading_context = io->ops.new_reading_context( io );
-    if ( reading_context == NULL )
-        goto err;
-
-    answer->i_body = io->size;
-    answer->p_body = malloc( io->size );
-    if ( unlikely( data == NULL ) )
-        goto err;
-
-    size_t to_read = io->size;
-    size_t cursor = 0;
-    while ( to_read != 0 )
-    {
-        const ssize_t read =
-            io->ops.read( reading_context, answer->p_body + cursor, to_read );
-
-        if ( read == -1 )
-        {
-            io->ops.release_reading_context( reading_context );
-            goto err;
-        }
-
-        cursor += read;
-        to_read -= read;
-    }
-
-    io->ops.release_reading_context( reading_context );
-
-    vlc_mutex_unlock( &p_sys->playlist_lock );
-
-    answer->i_proto = HTTPD_PROTO_HTTP;
-    answer->i_version = 0;
-    answer->i_type = HTTPD_MSG_ANSWER;
-    answer->i_status = 200;
-
-    httpd_MsgAdd( answer, "Content-type", "application/vnd.apple.mpegurl" );
-    httpd_MsgAdd( answer, "Cache-Control", "no-cache" );
-    httpd_MsgAdd( answer, "Connection", "close" );
-    httpd_MsgAdd( answer, "Access-Control-Allow-Origin", "*" );
-    httpd_MsgAdd( answer, "Access-Control-Expose-Headers", "Content-Length" );
-    return VLC_SUCCESS;
-
-err:
-    vlc_mutex_unlock( &p_sys->playlist_lock );
-    free( answer->p_body );
-    return VLC_EGENERIC;
-}
-
+                            const httpd_message_t *query );
 static int url_segment_cb( httpd_callback_sys_t *data,
                            httpd_client_t *cl,
                            httpd_message_t *answer,
-                           const httpd_message_t *query )
-{
-    if ( !answer || !query || !cl )
-        return VLC_SUCCESS;
-
-    const output_segment_t *segment = (output_segment_t *)data;
-    const hls_io *io = segment->io_handle;
-
-    printf( "fetching %s\n", segment->psz_filename );
-    ;
-
-    void *reading_context = io->ops.new_reading_context( io );
-    if ( reading_context == NULL )
-        return VLC_EGENERIC;
-
-    assert( io->size );
-    assert( segment->closed );
-
-    answer->p_body = malloc( io->size );
-
-    size_t to_read = io->size;
-    size_t cursor = 0;
-    while ( to_read != 0 )
-    {
-        const ssize_t read =
-            io->ops.read( reading_context, answer->p_body + cursor, to_read );
-
-        if ( read == -1 )
-            goto err;
-
-        cursor += read;
-        to_read -= read;
-    }
-
-    io->ops.release_reading_context( reading_context );
-    answer->i_body = cursor;
-
-    answer->i_proto = HTTPD_PROTO_HTTP;
-    answer->i_version = 0;
-    answer->i_type = HTTPD_MSG_ANSWER;
-    answer->i_status = 200;
-
-    httpd_MsgAdd( answer, "Content-type", "video/mpeg" );
-    httpd_MsgAdd( answer, "Cache-Control", "no-cache" );
-    httpd_MsgAdd( answer, "Connection", "close" );
-    httpd_MsgAdd( answer, "Access-Control-Allow-Origin", "*" );
-    httpd_MsgAdd( answer, "Access-Control-Expose-Headers", "Content-Length" );
-    return VLC_SUCCESS;
-err:
-    io->ops.release_reading_context( reading_context );
-    free( answer->p_body );
-    return VLC_EGENERIC;
-}
+                           const httpd_message_t *query );
 
 static int LoadCryptFile( sout_access_out_t *p_access );
 static int CryptSetup( sout_access_out_t *p_access, char *keyfile );
@@ -1719,4 +1607,121 @@ static hls_io *hls_io_NewBlockChain()
                         .unlink = blockchain_unlink,
                         .release = blockchain_release };
     return ret;
+}
+
+/*****************************************************************************
+ * HTTP: Utils
+ *****************************************************************************/
+
+static uint8_t *io_read_all( const hls_io *io )
+{
+
+    uint8_t *ret = malloc( io->size );
+    if ( unlikely( ret == NULL ) )
+        return NULL;
+
+    void *reading_context = io->ops.new_reading_context( io );
+    if ( reading_context == NULL )
+        goto err;
+
+    size_t to_read = io->size;
+    size_t cursor = 0;
+    while ( to_read != 0 )
+    {
+        const ssize_t read =
+            io->ops.read( reading_context, ret + cursor, to_read );
+
+        if ( read == -1 )
+        {
+            io->ops.release_reading_context( reading_context );
+            goto err;
+        }
+
+        cursor += read;
+        to_read -= read;
+    }
+
+    io->ops.release_reading_context( reading_context );
+    return ret;
+err:
+    free( ret );
+    return NULL;
+}
+
+static void set_http_headers( httpd_message_t *answer )
+{
+    httpd_MsgAdd( answer, "Content-type", "application/vnd.apple.mpegurl" );
+    httpd_MsgAdd( answer, "Cache-Control", "no-cache" );
+    httpd_MsgAdd( answer, "Connection", "close" );
+    httpd_MsgAdd( answer, "Access-Control-Allow-Origin", "*" );
+    httpd_MsgAdd( answer, "Access-Control-Expose-Headers", "Content-Length" );
+}
+
+/*****************************************************************************
+ * HTTP: URL callbacks
+ *****************************************************************************/
+
+static int url_playlist_cb( httpd_callback_sys_t *data,
+                            httpd_client_t *cl,
+                            httpd_message_t *answer,
+                            const httpd_message_t *query )
+{
+    if ( !answer || !query || !cl )
+        return VLC_SUCCESS;
+
+    sout_access_out_sys_t *p_sys = (sout_access_out_sys_t *)data;
+
+    const hls_io *io = p_sys->p_playlist;
+
+    vlc_mutex_lock( &p_sys->playlist_lock );
+
+    if ( io == NULL )
+        goto err;
+
+    answer->i_body = io->size;
+    answer->p_body = io_read_all( io );
+    if ( answer->p_body == NULL )
+        goto err;
+
+    vlc_mutex_unlock( &p_sys->playlist_lock );
+
+    answer->i_proto = HTTPD_PROTO_HTTP;
+    answer->i_version = 0;
+    answer->i_type = HTTPD_MSG_ANSWER;
+    answer->i_status = 200;
+
+    set_http_headers( answer );
+
+    return VLC_SUCCESS;
+
+err:
+    vlc_mutex_unlock( &p_sys->playlist_lock );
+    return VLC_EGENERIC;
+}
+
+static int url_segment_cb( httpd_callback_sys_t *data,
+                           httpd_client_t *cl,
+                           httpd_message_t *answer,
+                           const httpd_message_t *query )
+{
+    if ( !answer || !query || !cl )
+        return VLC_SUCCESS;
+
+    const output_segment_t *segment = (output_segment_t *)data;
+    const hls_io *io = segment->io_handle;
+
+    printf( "fetching %s\n", segment->psz_filename );
+
+    answer->i_body = io->size;
+    answer->p_body = io_read_all( io );
+    if ( answer->p_body == NULL )
+        return VLC_EGENERIC;
+
+    answer->i_proto = HTTPD_PROTO_HTTP;
+    answer->i_version = 0;
+    answer->i_type = HTTPD_MSG_ANSWER;
+    answer->i_status = 200;
+
+    set_http_headers( answer );
+    return VLC_SUCCESS;
 }
