e8f67dcf1ca8261658bfae6001c9d2cac02ceb92
Zhao Zhili <quinkblack@foxmail.com>
2022-04-15 19:53:51 +0800

    videotoolbox: ignore null imageBuffer and continue
    It can happen with corrupted streams and recover after that.

--- modules/codec/videotoolbox.c
+++ modules/codec/videotoolbox.c
@@ -2159,8 +2159,7 @@ static void DecoderCallback(void *decompressionOutputRefCon,
     }
     if (unlikely(!imageBuffer))
     {
-        msg_Err(p_dec, "critical: null imageBuffer with a valid status");
-        p_sys->vtsession_status = VTSESSION_STATUS_ABORT;
+        msg_Warn(p_dec, "null imageBuffer with a valid status");
         goto end;
     }
 
