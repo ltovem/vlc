ff3924a3628e601852790bb5f23af71b9fa61ed6
Steve Lhomme <robux4@ycbcr.xyz>
2022-07-04 11:26:37 +0200

    lua: return explicitly -1 on invalid file descriptor
    No need to try to map and otherwise close this invalid fd.

--- modules/lua/libs/net.c
+++ modules/lua/libs/net.c
@@ -239,7 +239,10 @@ static int vlclua_net_accept( lua_State *L )
     int **ppi_fd = (int**)luaL_checkudata( L, 1, "net_listen" );
     int i_fd = net_Accept( p_this, *ppi_fd );
 
-    lua_pushinteger( L, vlclua_fd_map_safe( L, i_fd ) );
+    if (i_fd < 0)
+        lua_pushinteger( L, -1 );
+    else
+        lua_pushinteger( L, vlclua_fd_map_safe( L, i_fd ) );
     return 1;
 }
 
@@ -252,7 +255,10 @@ static int vlclua_net_connect_tcp( lua_State *L )
     const char *psz_host = luaL_checkstring( L, 1 );
     int i_port = luaL_checkinteger( L, 2 );
     int i_fd = net_ConnectTCP( p_this, psz_host, i_port );
-    lua_pushinteger( L, vlclua_fd_map_safe( L, i_fd ) );
+    if (i_fd < 0)
+        lua_pushinteger( L, -1 );
+    else
+        lua_pushinteger( L, vlclua_fd_map_safe( L, i_fd ) );
     return 1;
 }
 
