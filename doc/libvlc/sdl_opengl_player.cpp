f543ebe13369e027e63ce456465b97098c84d018
Pierre Lamot <pierre@videolabs.io>
2023-05-16 16:12:01 +0200

    doc: sdl_opengl: update SDL initialisation

--- doc/libvlc/sdl_opengl_player.cpp
+++ doc/libvlc/sdl_opengl_player.cpp
@@ -53,9 +53,6 @@ public:
             "--verbose=4"
         };
         m_vlc = libvlc_new(sizeof(args) / sizeof(*args), args);
-
-        //VLC opengl context needs to be shared with SDL context
-        SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 1);
         m_ctx = SDL_GL_CreateContext(window);
     }
 
@@ -238,15 +235,22 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    //VLC opengl context needs to be shared with SDL context
+    SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 1);
+    SDL_GL_SetSwapInterval(0);
+
     SDL_Window* wnd = SDL_CreateWindow("test",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetSwapInterval(0);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
 
     SDL_GLContext glc = SDL_GL_CreateContext(wnd);
     SDL_GL_MakeCurrent(wnd, glc);
