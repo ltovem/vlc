e8ed302bfcc9c9c85284cc9376342cc1a3122bed
Alexandre Janniaux <ajanni@videolabs.io>
2023-06-23 19:37:30 +0200

    misc: subpicture: use new operation table

--- src/misc/subpicture.c
+++ src/misc/subpicture.c
@@ -70,11 +70,11 @@ subpicture_t *subpicture_New( const subpicture_updater_t *p_upd )
     else
     {
         p_subpic->p_private = NULL;
-
         p_subpic->updater.pf_validate = NULL;
         p_subpic->updater.pf_update   = NULL;
         p_subpic->updater.pf_destroy  = NULL;
-        p_subpic->updater.sys         = NULL;
+        p_subpic->updater.ops = NULL;
+        p_subpic->updater.sys = NULL;
     }
     return p_subpic;
 }
@@ -86,6 +86,11 @@ void subpicture_Delete( subpicture_t *p_subpic )
 
     if( p_subpic->updater.pf_destroy )
         p_subpic->updater.pf_destroy( p_subpic );
+    else if (p_subpic->updater.ops != NULL &&
+        p_subpic->updater.ops->destroy != NULL)
+    {
+        p_subpic->updater.ops->destroy(p_subpic);
+    }
 
     if( p_subpic->p_private )
     {
@@ -153,20 +158,30 @@ void subpicture_Update( subpicture_t *p_subpicture,
     subpicture_updater_t *p_upd = &p_subpicture->updater;
     subpicture_private_t *p_private = p_subpicture->p_private;
 
-    if( !p_upd->pf_validate )
-        return;
-    if( !p_upd->pf_validate( p_subpicture,
-                          !video_format_IsSimilar( p_fmt_src,
-                                                   &p_private->src ), p_fmt_src,
-                          !video_format_IsSimilar( p_fmt_dst,
-                                                   &p_private->dst ), p_fmt_dst,
-                          i_ts ) )
-        return;
+    if (p_upd->pf_validate != NULL)
+    {
+        if (!p_upd->pf_validate(p_subpicture,
+               !video_format_IsSimilar(p_fmt_src, &p_private->src), p_fmt_src,
+               !video_format_IsSimilar(p_fmt_dst, &p_private->dst), p_fmt_dst,
+               i_ts))
+            return;
+    }
+    else if (p_upd->ops != NULL || p_upd->ops->validate != NULL)
+    {
+        if (!p_upd->ops->validate(p_subpicture,
+                !video_format_IsSimilar( p_fmt_src, &p_private->src ), p_fmt_src,
+                !video_format_IsSimilar( p_fmt_dst, &p_private->dst ), p_fmt_dst,
+                i_ts))
+            return;
+    }
 
     subpicture_region_ChainDelete( p_subpicture->p_region );
     p_subpicture->p_region = NULL;
 
-    p_upd->pf_update( p_subpicture, p_fmt_src, p_fmt_dst, i_ts );
+    if (p_upd->pf_update != NULL)
+        p_upd->pf_update(p_subpicture, p_fmt_src, p_fmt_dst, i_ts);
+    else
+        p_upd->ops->update(p_subpicture, p_fmt_src, p_fmt_dst, i_ts);
 
     video_format_Clean( &p_private->src );
     video_format_Clean( &p_private->dst );
