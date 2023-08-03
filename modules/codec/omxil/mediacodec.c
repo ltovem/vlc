e6239fa49448fa3d598d4dc4397ad3c368f1b786
Thomas Guillem <thomas@gllm.fr>
2023-08-03 15:48:02 +0200

    mediacodec: fix possible video freeze in case of corrupted input
    Fixes #28339

--- modules/codec/omxil/mediacodec.c
+++ modules/codec/omxil/mediacodec.c
@@ -122,6 +122,7 @@ typedef struct decoder_sys_t
 
     int             i_decode_flags;
 
+    vlc_tick_t      max_ts;
     enum es_format_category_e cat;
     union
     {
@@ -849,6 +850,7 @@ static int OpenDecoder(vlc_object_t *p_this, pf_MediaCodecApi_init pf_init)
     p_sys->api.i_cat = p_dec->fmt_in->i_cat;
     p_sys->api.psz_mime = mime;
     p_sys->b_decoder_dead = false;
+    p_sys->max_ts = VLC_TICK_0;
 
     if (pf_init(&p_sys->api) != 0)
     {
@@ -1137,6 +1139,16 @@ static int Video_ProcessOutput(decoder_t *p_dec, mc_api_out *p_out,
             p_pic->date = p_out->buf.i_ts;
         else
             p_pic->date = forced_ts;
+
+        if (p_pic->date > p_sys->max_ts)
+        {
+            msg_Warn(p_dec, "Dropping picture with invalid ts"
+                     "(pic_ts=%"PRId64" vs last_input_ts=%"PRId64")",
+                     p_pic->date, p_sys->max_ts);
+            picture_Release(p_pic);
+            return p_sys->api.release_out(&p_sys->api, p_out->buf.i_index, false);
+        }
+
         p_pic->b_progressive = true;
 
         if (p_sys->api.b_direct_rendering)
@@ -1559,6 +1571,8 @@ static int QueueBlockLocked(decoder_t *p_dec, block_t *p_in_block,
                 }
                 p_buf = p_block->p_buffer;
                 i_size = p_block->i_buffer;
+                if (i_ts > p_sys->max_ts)
+                    p_sys->max_ts = i_ts;
             }
 
             if (p_sys->api.queue_in(&p_sys->api, i_index, p_buf, i_size,
