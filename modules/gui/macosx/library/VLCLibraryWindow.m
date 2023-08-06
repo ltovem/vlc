2b8e1c350bdcd6e47a7a6f54b45f59bfbc660d45
Claudio Cambra <developer@claudiocambra.com>
2023-08-06 15:09:52 +0800

    macosx: Store and retrieve playlsit view mode correctly from persistent preferences in library window
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCLibraryWindow.m
+++ modules/gui/macosx/library/VLCLibraryWindow.m
@@ -406,6 +406,9 @@ static void addShadow(NSImageView *__unsafe_unretained imageView)
     case VLCLibrarySongsMusicSubSegment:
         _currentSelectedViewModeSegment = preferences.songsLibraryViewMode;
         break;
+    case VLCLibraryPlaylistsSegment:
+        _currentSelectedViewModeSegment = preferences.playlistLibraryViewMode;
+        break;
     case VLCLibraryBrowseSegment:
         _currentSelectedViewModeSegment = preferences.browseLibraryViewMode;
         break;
@@ -489,6 +492,9 @@ static void addShadow(NSImageView *__unsafe_unretained imageView)
         break;
     case VLCLibrarySongsMusicSubSegment:
         preferences.songsLibraryViewMode = _currentSelectedViewModeSegment;
+        break;
+    case VLCLibraryPlaylistsSegment:
+        preferences.playlistLibraryViewMode = _currentSelectedViewModeSegment;
         break;
     case VLCLibraryBrowseSegment:
         preferences.browseLibraryViewMode = _currentSelectedViewModeSegment;
