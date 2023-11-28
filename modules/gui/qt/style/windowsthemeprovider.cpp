a305c6c31a4791d74e234a0c3fe08145261fcbb1
Steve Lhomme <robux4@ycbcr.xyz>
2023-11-28 13:12:07 +0100

    qt: fix potential mixing of char/WCHAR comparison
    WM_SETTINGCHANGE provides a CHAR/WCHAR string depending on UNICODE ??

--- modules/gui/qt/style/windowsthemeprovider.cpp
+++ modules/gui/qt/style/windowsthemeprovider.cpp
@@ -53,11 +53,16 @@ public:
     bool nativeEventFilter(const QByteArray &, void *message, long *)
     {
         MSG* msg = static_cast<MSG*>( message );
-        if ( msg->message == WM_SETTINGCHANGE
-             && !lstrcmp( LPCTSTR( msg->lParam ), L"ImmersiveColorSet" ) )
+        if ( msg->message == WM_SETTINGCHANGE )
         {
-            if (m_obj->paletteUpdated)
-                m_obj->paletteUpdated(m_obj, m_obj->paletteUpdatedData);
+            LPCTSTR setting = LPCTSTR( msg->lParam );
+#ifdef UNICODE
+            if (!wcscmp( setting, L"ImmersiveColorSet" ) )
+#else
+            if (!strcmp( setting, "ImmersiveColorSet" ) )
+#endif
+                if (m_obj->paletteUpdated)
+                    m_obj->paletteUpdated(m_obj, m_obj->paletteUpdatedData);
         }
         else if (msg->message == WM_SYSCOLORCHANGE)
         {
