3e7fe18707481e679455a74c1bb9d9951cc56dd8
Thomas Guillem <thomas@gllm.fr>
2023-11-29 16:15:12 +0100

    lib: media_player: expose structs from on_chapter_selection_changed()

--- include/vlc/libvlc_media_player.h
+++ include/vlc/libvlc_media_player.h
@@ -75,7 +75,7 @@ typedef struct libvlc_title_description_t
 typedef struct libvlc_chapter_description_t
 {
     int64_t i_time_offset; /**< time-offset of the chapter in milliseconds */
-    int64_t i_duration; /**< duration of the chapter in milliseconds */
+    int64_t i_duration; /**< duration of the chapter in milliseconds, 0 if unknown */
     char *psz_name; /**< chapter name */
 } libvlc_chapter_description_t;
 
@@ -393,11 +393,18 @@ struct libvlc_media_player_cbs
      * available since \ref LIBVLC_MEDIA_PLAYER_CBS_VER_0
      *
      * \param opaque opaque pointer set by libvlc_media_player_new()
+     * \param title description of the new selected title, valid only from this
+     * callback, do not release
      * \param title_idx index of the title
+     * \param chapter description of the new selected chapter, valid only from this
+     * callback, do not release
      * \param chapter_idx index of the new selected chapter
      */
     void (*on_chapter_selection_changed)( void *opaque,
-                                          unsigned title_idx, unsigned chapter_idx );
+                                          const libvlc_title_description_t *title,
+                                          unsigned title_idx,
+                                          const libvlc_chapter_description_t *chapter,
+                                          unsigned chapter_idx );
 
     /**
      * Callback prototype that notify when the player recording state changed
