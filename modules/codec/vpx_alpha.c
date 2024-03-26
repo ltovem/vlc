d98786bdf414d0598f9b668e8b876ba1e0fa7723
Thomas Guillem <thomas@gllm.fr>
2024-03-26 17:34:15 +0100

    WIP: codec: pass clock_id to get_display_date
    TODO: can't use VLC_CLOCK_ID_LAST since private API.

--- modules/codec/vpx_alpha.c
+++ modules/codec/vpx_alpha.c
@@ -325,10 +325,10 @@ static void QueuePic( decoder_t *dec, picture_t *pic )
     vlc_mutex_unlock(&p_sys->lock);
 }
 
-static vlc_tick_t GetDisplayDate( decoder_t *dec, vlc_tick_t sys_now, vlc_tick_t ts)
+static vlc_tick_t GetDisplayDate( decoder_t *dec, uint32_t clock_id, vlc_tick_t sys_now, vlc_tick_t ts)
 {
     decoder_t *bdec = container_of(vlc_object_parent(dec), decoder_t, obj);
-    return decoder_GetDisplayDate(bdec, sys_now, ts);
+    return decoder_GetDisplayDate(bdec, clock_id, sys_now, ts);
 }
 
 static float GetDisplayRate( decoder_t *dec )
