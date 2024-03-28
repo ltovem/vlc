59a10736b0ad6b1c2c4a40e3c0a4059acf87a1dd
Fatih Uzunoglu <fuzun54@outlook.com>
2024-03-28 16:43:41 +0200

    qt: add thread busy property to MediaLib

--- modules/gui/qt/medialibrary/medialib.cpp
+++ modules/gui/qt/medialibrary/medialib.cpp
@@ -510,6 +510,8 @@ void MediaLib::cancelMLTask(const QObject* object, quint64 taskId)
     if (removed)
         delete task;
     m_runningTasks.remove(taskId);
+    if (m_runningTasks.isEmpty())
+        emit threadBusyChanged();
     m_objectTasks.remove(object, taskId);
     if (m_objectTasks.count(object) == 0)
         disconnect(object, &QObject::destroyed, this, &MediaLib::runOnMLThreadTargetDestroyed);
@@ -534,6 +536,8 @@ void MediaLib::runOnMLThreadDone(RunOnMLThreadBaseRunner* runner, quint64 target
         if (status == ML_TASK_STATUS_SUCCEED)
             runner->runUICallback();
         m_runningTasks.remove(target);
+        if (m_runningTasks.isEmpty())
+            emit threadBusyChanged();
         m_objectTasks.remove(object, target);
         if (m_objectTasks.count(object) == 0)
             disconnect(object, &QObject::destroyed, this, &MediaLib::runOnMLThreadTargetDestroyed);
@@ -554,6 +558,8 @@ void MediaLib::runOnMLThreadTargetDestroyed(QObject * object)
                 delete task;
             m_runningTasks.remove(taskId);
         }
+        if (m_runningTasks.isEmpty())
+            emit threadBusyChanged();
         m_objectTasks.remove(object);
         //no need to disconnect QObject::destroyed, as object is currently being destroyed
     }
